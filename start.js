055265d8d71a5f0eecbe586e68cd3fdea03e172f
banuakman <banu@asterya.com>
2021-02-17 10:00:49 -0500

    add initial questions

--- start.js
+++ start.js
@@ -1,6 +1,32 @@
+// DEPENDENCIES ================================
 const inquirer = require('inquirer');
 const connection = require("./db/connection.js");
 const CFonts = require('cfonts');
+
+// DATA =======================================
+const initQs = [
+    // Menu items
+    {
+       type: "list",
+       name: "initialSelection",
+       message: "What would you like to do?",
+       choices: ["View all employees",
+               "View all employees by department",
+               "View all employees by manager",
+               "Add an employee",
+               "Remove an employee",
+               "Update employee role",
+               "Update employee manager",
+               "View all departments",
+               "Add a department",
+               "View all roles",
+               "Add role",
+               "Exit"
+           ]
+   }
+]
+
+// FUNCTIONS ==================================
 
 CFonts.say ("Employee Tracker", {
     font: 'pallet',
@@ -13,4 +39,4 @@ connection.connect((err) => {
     connection.end();
   });
 
-
+// USER INTERACTIONS ==========================
