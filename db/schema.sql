aef4d6382b0a8d72726a5835558e1d9e54c42561
banuakman <banu@asterya.com>
2021-02-16 22:48:59 -0500

    create roles & employee schema

--- db/schema.sql
+++ db/schema.sql
@@ -11,3 +11,31 @@ CREATE TABLE department (
     PRIMARY KEY (id)
 );
 
+--ROLES
+CREATE TABLE role (
+    id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT NULL,
+    PRIMARY KEY (id),
+    CONSTRAINT fk_department_id
+        FOREIGN KEY (department_id)
+        REFERENCES department(id)
+            ON UPDATE CASCADE
+);
+
+-- EMPLOYEE
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    manager_id INT NULL,
+    role_id INT NULL,
+    PRIMARY KEY (id),
+    CONSTRAINT fk_role_id
+        FOREIGN KEY (role_id)
+        REFERENCES role(id)
+            ON UPDATE CASCADE,
+);
+
+
