7dfe8b0c3c21150fb622ae1bb01f55f7c73db57e
banuakman <banu@asterya.com>
2021-02-16 20:10:13 -0500

    create initial files

---
+++ db/connection.js
@@ -0,0 +1,16 @@
+require('dotenv').config()
+
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  // Your port; if not 3306
+  port: 3306,
+  // Your username
+  user: 'root',
+  // update with your own MySQL password!
+  password: process.env.DB_PASSWORD,
+  database: 'employee_db',
+});
+
+module.exports = connection;
